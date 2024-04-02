6603ab4a07c49f7df7e883bcab13b4f757199899
Mace Akkari <mace.akkari@relyfe.com>
2024-04-02 14:34:03 +0100

    add priority

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,19 +1,33 @@
 import { Task } from "../utils/data-tasks"
 
+const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
+</svg>
+const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
+</svg>
+const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+</svg>
+
 const TaskCard = ({task}:{
   task: Task
   }) => {
-  return (
-  <div className="border rounded-lg px-2 m-2 bg-gray-50">
-      <div className="text-base font-semibold py-2">
+  return <div className="border rounded-lg px-2 m-2 bg-gray-50 w-30">
+      <div className="text-base font-base py-2">
         {task.title}
       </div>
-      <div className="flex gap-4 justify-between py-2 text-gray-800">
-        <div>{task.id}</div>
+      <div className="flex gap-4 justify-between py-2 text-gray-600 text-sm">
+        <div className="flex gap-2">
+          <div>{task.id}</div>
+          {task.priority === 'high' && highPriorityIcon}
+          {task.priority === 'medium' && mediumPriorityIcon}
+          {task.priority === 'low' && lowPriorityIcon}
+        </div>
         <div>{task.points}</div>
       </div>
     </div>
-  )
 }
 
 export default TaskCard
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,18 +1,35 @@
+export type Status = 'Todo' | 'Inprogress' | 'Done'
+export type Priority = 'low' | 'medium' | 'high'
 export type Task= {
   title: string, 
   id: string, 
+  status: Status,
+  priority: Priority
   points?: number
 }
 
+export const statuses: Status[] =  ['Todo', 'Inprogress', 'Done']
+export const priorities: Priority[] = ['low' , 'medium' , 'high']
 export const tasks: Task[] = [
   {
     title: 'A task',
     id: 'BUS-1',
+    status: 'Todo',
+    priority: 'medium',
     points:  5,
   },
   {
     title: 'Another task',
+    id: 'BUS-3',
+    status: 'Inprogress',
+    priority: 'high',
+    points:  7,
+  },
+  {
+    title: 'Another Another task',
     id: 'BUS-2',
+    status: 'Done',
+    priority: 'medium',
     points:  7,
   },
 ]
