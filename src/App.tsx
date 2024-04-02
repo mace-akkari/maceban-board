ffd5feb322805707a8056459129a01d25c5f1313
Mace Akkari <mace.akkari@relyfe.com>
2024-04-02 13:25:01 +0100

    refactoring of code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import './App.css'
-import TaskCard from './components/TaskCard'
-import { Task } from './utils/data-tasks';
+import './App.css';
+import TaskCard from './components/TaskCard';
+import { tasks } from './utils/data-tasks';
+
 function App() {
-  const task: Task = {
-    title: 'A task',
-    id: 'BUS-1',
-    points:  5,
-  }
 
   return (
     <>
-    <TaskCard task={task} />
+    {tasks.map((task) => <TaskCard task={task} />)}
     </>
   )
 }
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,5 +1,18 @@
 export type Task= {
   title: string, 
   id: string, 
-  points: number
+  points?: number
 }
+
+export const tasks: Task[] = [
+  {
+    title: 'A task',
+    id: 'BUS-1',
+    points:  5,
+  },
+  {
+    title: 'Another task',
+    id: 'BUS-2',
+    points:  7,
+  },
+]
